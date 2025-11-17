a6ff036c2fe039ef1f866b360d1c9b5e7f64b14d
Manasseh David <manassehdavid124@gmail.com>
2025-11-17 11:27:33 +0100

    final

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ import AddTask from './components/AddTask';
 const page = () => {
   return (
     <div>
+      <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' rel='stylesheet'></link>
       <Header />
       <AddTask />
       
